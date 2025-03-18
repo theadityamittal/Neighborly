ae8650af70d22aadc55866902d74634718585a86
Chancellor Richey <104276582+ChancellorO@users.noreply.github.com>
2025-03-18 13:38:21 -0400

    Update store.js.

--- react-app/src/redux/store.js
+++ react-app/src/redux/store.js
@@ -1,27 +1,11 @@
 import { combineReducers, createStore } from 'redux';
 import { authSlice } from './authSlice';
 import { persistReducer, persistStore } from 'redux-persist';
-
-function defaultReplacer(key, replaceCharacter) {
-    return key.replace(/[^a-zA-Z0-9\-_]/g, replaceCharacter);
-}
-
-function createSecureStore(options = {}) {
-    const replace_Character = "_";
-    const replacer = defaultReplacer;
-    
-    return {
-        getItem: (key) => createSecureStore.getItemAsync(replacer(key, replace_Character), options),
-        setItem: (key, value) => createSecureStore.setItemAsync(replacer(key, replace_Character), value, options),
-        removeItem: (key) => createSecureStore.removeItemAsync(replacer(key, replace_Character), options),
-    };
-}
-
-const secureStorage = createSecureStore();
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
     key: 'auth',
-    storage: secureStorage,
+    storage,
 };
 
 const rootReducer = combineReducers({
