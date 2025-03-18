652822dcf5d0c481b8441560f58d6ebc57da5295
Aditya Mittal <43758058+theadityamittal@users.noreply.github.com>
2025-03-18 20:39:57 +0000

    Fix import statement for react-router in AppRoutes.js to use react-router-dom

--- react-app/src/routes/AppRoutes.js
+++ react-app/src/routes/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Tools from "../pages/Tools";
