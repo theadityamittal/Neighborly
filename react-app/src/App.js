32fab573aa9e4ccbef18759bb8486460bbf9fcc3
Aditya Mittal <43758058+theadityamittal@users.noreply.github.com>
2025-03-18 13:56:50 +0000

    Refactor import statement in App.js to use 'react-router' instead of 'react-router-dom'

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router';
 import Test from './screens/Test';
 
 function App() {
