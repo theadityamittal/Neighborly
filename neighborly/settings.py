7e8e4e3c21692f6ea0d643e0d0df38ab1cd4867e
Aditya Mittal <43758058+theadityamittal@users.noreply.github.com>
2025-04-11 02:23:44 +0000

    Update CORS settings to allow all origins for development

--- neighborly/settings.py
+++ neighborly/settings.py
@@ -73,10 +73,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://3.145.158.84"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://3.145.158.84"
+# ]
 
 ROOT_URLCONF = 'neighborly.urls'
 
