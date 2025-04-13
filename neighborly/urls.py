a3def1e1aabf245b5b823c2cf71add3c7b280557
cathytsui <48180241+cathy-thy@users.noreply.github.com>
2025-04-13 10:07:42 -0400

    Add api docs

--- neighborly/urls.py
+++ neighborly/urls.py
@@ -1,6 +1,21 @@
 from neighborly_users.admin import admin_site
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Neighborly Backend APIs",
+      default_version='v1',
+      description="This is the API documentation for Desphixs LMS project APIs",
+      terms_of_service="https://www.google.com/policies/terms/"
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('auth/', include('neighborly_users.urls')),
@@ -8,4 +23,9 @@ urlpatterns = [
     path('petitions/', include('neighborly_petitions.urls')),
     path('events/', include('neighborly_events.urls')),
     path('', include('apps.core.urls')),
+
+    #api documentation
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api_doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
