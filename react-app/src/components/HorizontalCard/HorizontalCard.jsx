a84acfbe13dc5f48953080608d8d4354dbd9bb0a
cathytsui <48180241+cathy-thy@users.noreply.github.com>
2025-04-15 01:53:46 -0400

    update datepicker

--- react-app/src/components/HorizontalCard/HorizontalCard.jsx
+++ react-app/src/components/HorizontalCard/HorizontalCard.jsx
@@ -12,7 +12,6 @@ function HorizontalCard({
   viewType,
   onView // this is passed from CardTest to trigger showing VerticalCard
 }) {
-  // console.log("HorizontalCard props:", {id, title, provider, location, closestAvailability, image, tabs, viewType, onView});
   const handleClick = () => {
     console.log(`You clicked on card ID: ${id}`);
     if (onView) {
