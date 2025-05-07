188fc1c39168315886091235da50db3d0d945abc
Lakshmi Shibu Raj <lakshmi.shiburaj@gmail.com>
2025-05-06 21:26:39 -0400

    Add tests for grabbing events by organizer and tools by owner;

--- neighborly_events/tests.py
+++ neighborly_events/tests.py
@@ -1,44 +1,61 @@
-# from django.test import TestCase
-# from rest_framework.test import APITestCase
-# from django.urls import reverse
-# from rest_framework import status
 
-# class PetitionTests(APITestCase):
-    
-#     def setUp(self):
-#         self.register_url = reverse('register')
-#         self.grab_event_data = reverse('grabEventsData')
-#         self.login_url = reverse('token_obtain_pair')
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from neighborly_events.models import Event
 
-#         self.user_data = {
-#             "name": "Steve Harvey",
-#             "email": "steveharvey@example.com",
-#             "phone_number": "1234567890",
-#             "address": "123 Street, City",
-#             "neighborhood": "Brooklyn",
-#             "account_type": "customer",
-#             "password": "password123"
-#         }
+User = get_user_model()
 
-#     def authenticate_user(self):
-#         register_response = self.client.post(self.register_url, self.user_data, format='json')
-#         self.assertEqual(register_response.status_code, status.HTTP_201_CREATED)
+class GrabEventsByOrganizerTests(APITestCase):
 
-#         login_response = self.client.post(self.login_url, {
-#             "email": self.user_data["email"],
-#             "password": self.user_data["password"]
-#         }, format='json')
+    def setUp(self):
+        # Create a user
+        self.user = User.objects.create_user(
+        name='Test User',
+        email='testuser@example.com',
+        phone_number='1234567890',
+        address='123 Test St',
+        neighborhood='Test Neighborhood',
+        account_type='customer',
+        city='Test City',           
+        zip_code='12345',           
+        password='testpassword'
+        )
+        self.event = Event.objects.create(
+        event_name='Test Event',
+        organizer_name=self.user.name,     
+        organizer_id=str(self.user.id),    
+        description='This is a test event.',
+        location='Test Location',
+        date='2024-05-01',
+        time='14:00:00',
+        visibility='public',
+        tags=['test', 'event'],
+        )
 
-#         self.assertEqual(login_response.status_code, status.HTTP_200_OK)
-#         self.assertIn("access", login_response.data)
-#         return login_response.data["access"]
+        # Login to get the access token
+        login_url = reverse('token_obtain_pair')
+        response = self.client.post(login_url, {
+            'email': 'testuser@example.com',
+            'password': 'testpassword'
+        }, format='json')
 
-#     def test_grab_event_data_test(self):
-#         access_token = self.authenticate_user()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = response.data['access']
 
-#         response = self.client.get(
-#             self.grab_event_data,
-#             HTTP_AUTHORIZATION=f'Bearer {access_token}'
-#         )
+    def test_grab_events_by_organizer(self):
+        url = f'/events/grabEventsData/organizer/{self.user.id}'
+        
+        response = self.client.get(
+            url,
+            HTTP_AUTHORIZATION=f'Bearer {self.token}'
+        )
 
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('events', response.data)
+        self.assertEqual(len(response.data['events']), 1)
+
+        event_data = response.data['events'][0]
+        self.assertEqual(event_data['event_name'], 'Test Event')
+        self.assertEqual(event_data['location'], 'Test Location')
