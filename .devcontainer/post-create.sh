53c88adc0444022476bc818d30e3ab8205fc8ef8
Aditya Mittal <43758058+theadityamittal@users.noreply.github.com>
2025-05-21 12:36:29 -0400

    remove sensitive environment variables from .env file (#101)
    * remove sensitive environment variables from .env file
    
    * add .env file with fake configuration settings
    
    * fix: standardize environment variable naming conventions in .env file
    
    * fix: standardize environment variable naming conventions in AWS settings

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,10 +4,10 @@ npm install --prefix react-app &&
 start_servers.sh
 
 cat <<EOF > .env
-access_key=fake-access-key
-secret_key=fake-secret-key
-bucket_name=fake-bucket
-region_name=us-east-1
+ACCESS_KEY=your_aws_access_key_here
+SECRET_KEY=your_aws_secret_key_here
+BUCKET_NAME=your_bucket_name_here
+REGION_NAME=your_aws_region_here
 DJANGO_ENV=development
 DB_NAME=neighborly_db
 DB_USER=neighborly_user
